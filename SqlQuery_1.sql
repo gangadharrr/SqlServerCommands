9842beed402ea43d21401c1152bc6421d2a0f988
Gangadhar C <gangadharganga90@gmail.com>
2023-04-10 09:04:51 +0530

    database backup files and scripts

---
+++ SqlQuery_1.sql
@@ -0,0 +1,14 @@
+﻿--create database "College Sports Management System";
+
+
+DROP TABLE TOURNAMENTS;
+DROP TABLE players;
+DROP TABLE sports;
+DROP TABLE SCOREBOARD;
+DROP TABLE REGISTRATIONS;
+
+select * from sports;
+select * from players;
+select * from TOURNAMENTS;
+select * from SCOREBOARD;
+select * from REGISTRATIONS;
